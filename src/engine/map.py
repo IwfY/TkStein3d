66bbb6c3eb83f4492551a164cc7cb1d97ae8e218
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2012-08-27 11:45:43 +0200

    test map

--- src/engine/map.py
+++ src/engine/map.py
@@ -5,15 +5,22 @@ class Map(object):
         self.objects = []
         
         self.edgeLength = 15
+        
         # test input
-        self.objects.append(Block(2, 3, self.edgeLength))
-        self.objects.append(Block(1, 3, self.edgeLength))
-        self.objects.append(Block(-2, 3, self.edgeLength))
-        self.objects.append(Block(-1, 3, self.edgeLength))
-        self.objects.append(Block(0, 3, self.edgeLength))
-        self.objects.append(Block(2, 2, self.edgeLength))
-        self.objects.append(Block(-2, 2, self.edgeLength))
-        self.objects.append(Block(-2, 1, self.edgeLength))
+        grid = [[1, 1, 1, 1, 1, 1, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 1, 0, 0, 1],
+                [1, 1, 0, 1, 0, 0, 1],
+                [1, 0, 0, 1, 1, 1, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 0, 0, 0, 0, 0, 1],
+                [1, 1, 1, 1, 1, 1, 1]                
+                ]
+        i = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    self.objects.append(Block(i, j, self.edgeLength))
     
     
     def getObjects(self):
