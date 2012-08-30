847a9cca104eae804e7edbfccc60702d0f98dc5a
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2012-08-30 11:21:32 +0200

    SVGMap: parse translation, starting postition

--- src/engine/svgmap.py
+++ src/engine/svgmap.py
@@ -1,7 +1,8 @@
-from engine.coordinate import Point3D
+from engine.coordinate import Point3D, Vector3D
 from engine.map import Map
 from engine.polygon import Polygon
 
+import re
 from threading import Lock
 import xml.dom.minidom as dom
 
@@ -14,6 +15,7 @@ class SVGMap(Map):
         
         self.wallBottom = -7.5
         self.wallTop = 7.5
+        self.startPosition = Point3D(0.0, 0.0, 0.0)
         
         self.loadMap(svgFilename)
     
@@ -22,21 +24,39 @@ class SVGMap(Map):
         
         for path in xml.getElementsByTagName('path'):
             coordinateString = path.getAttribute('d')
+            translateVector = Vector3D(0.0, 0.0, 0.0)
+            transformString = path.getAttribute('transform')
+            if transformString is not None:
+                if transformString.startswith('translate'):
+                    rePathTranslate = re.compile('translate\(([0-9.]+),([0-9.]+)\)')
+                    matches = rePathTranslate.match(transformString)
+                    if matches is not None:
+                        matchGroups = matches.groups()
+                        translateVector.x = float(matchGroups[0])
+                        translateVector.z = -float(matchGroups[1])
+            
+            if path.getAttribute('sodipodi:type') == 'arc': # start position
+                x = float(path.getAttribute('sodipodi:cx'))
+                z = -float(path.getAttribute('sodipodi:cy'))
+                self.startPosition = Point3D(x + translateVector.x,
+                                             0.0,
+                                             z + translateVector.z)
+                continue
+            
             parts = coordinateString.split(' ')
-            x = float(parts[1].split(',')[0])
-            z = float(parts[1].split(',')[1])
+            x = float(parts[1].split(',')[0]) + translateVector.x
+            z = -float(parts[1].split(',')[1]) + translateVector.z
             if parts[0] == 'm':     #relative move
                 xDelta = float(parts[2].split(',')[0])
-                zDelta = float(parts[2].split(',')[1])
+                zDelta = -float(parts[2].split(',')[1])
             elif parts[0] == 'M':     #absolute move
-                xDelta = float(parts[2].split(',')[0]) - x
-                zDelta = float(parts[2].split(',')[1]) - z
+                xDelta = float(parts[2].split(',')[0]) - x + translateVector.x
+                zDelta = -float(parts[2].split(',')[1]) - z + translateVector.z
             else:
                 print('something went horribly wrong')
                 continue
             
-            print(x, z, xDelta, zDelta)
-            self.addWall(x, -z, xDelta, -zDelta)
+            self.addWall(x, z, xDelta, zDelta)
         
         
     def addWall(self, x, z, xDelta, zDelta, fill='grey', outline='darkgrey'):
@@ -59,3 +79,6 @@ class SVGMap(Map):
                              fill, outline)
         
         self.polygons.append(newPolygon)
+    
+    def getStartPosition(self):
+        return self.startPosition
