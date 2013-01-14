a2f9246fc3066c55d9865273967560e9fef34e2d
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2013-01-14 21:54:01 +0100

    first try using shaders in main program

--- src/engine/client/pygameviewandinput/pygameviewandinput.py
+++ src/engine/client/pygameviewandinput/pygameviewandinput.py
@@ -2,7 +2,11 @@ from engine.coordinate import Vector3D, Point3D
 from engine.polygon import moveAndRotatePolygon
 from engine.shared.utils import runAndWait, mixColors
 
+from numpy import append, array, float32
 from OpenGL.GL import *
+from OpenGL.GL.ARB.shader_objects import *
+from OpenGL.GL.ARB.vertex_shader import *
+from OpenGL.GL.ARB.fragment_shader import *
 from OpenGL.GLU import *
 import pygame
 from pygame.locals import *
@@ -25,6 +29,37 @@ class PygameViewAndInput(Thread):
         self.keysPressed = []
         
         self.eye = Point3D(0.0, 0.0, -2.0)
+        
+        self.staticVerticesCount = 0
+        self.attributeColorIndex = None
+        self.vertexShaderId = None
+        self.fragmentShaderId = None
+        self.programId = None
+        self.vaoId = None
+        self.vboId = None
+        self.colorBufferId = None
+        
+        self.vertexShader120 = '''
+#version 120
+
+attribute vec4 in_color;
+varying vec4 ex_color;
+
+void main(void) {
+    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    ex_color = in_color;
+}
+'''
+
+        self.fragmentShader120 = '''
+#version 120
+
+varying vec4 ex_color;
+
+void main() {
+    gl_FragColor = ex_color;
+}
+'''
     
     def resize(self, tuple_wh):
         width, height = tuple_wh
@@ -44,7 +79,133 @@ class PygameViewAndInput(Thread):
         glEnable(GL_DEPTH_TEST)
         glDepthFunc(GL_LEQUAL)
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+        
+        self.createShaders()
+        self.initStaticPolygonVBO()
     
+    def cleanup(self):
+        self.destroyShaders()
+        self.destroyStaticPolygonsVBO()
+    
+    def createShaders(self):
+        errorCheckValue = glGetError()
+         
+        self.vertexShaderId = glCreateShaderObjectARB(GL_VERTEX_SHADER)
+        glShaderSourceARB(self.vertexShaderId, [self.vertexShader120])
+        glCompileShaderARB(self.vertexShaderId)
+        
+        log = glGetInfoLogARB(self.vertexShaderId)
+        if log: print('Vertex Shader:', log)
+     
+        self.fragmentShaderId = glCreateShaderObjectARB(GL_FRAGMENT_SHADER)
+        glShaderSourceARB(self.fragmentShaderId, [self.fragmentShader120])
+        glCompileShaderARB(self.fragmentShaderId)
+        
+        log = glGetInfoLogARB(self.fragmentShaderId)
+        if log: print('Fragment Shader:', log)
+     
+        self.programId = glCreateProgramObjectARB()
+        glAttachObjectARB(self.programId, self.vertexShaderId)
+        glAttachObjectARB(self.programId, self.fragmentShaderId)
+        glLinkProgramARB(self.programId)
+        
+        log = glGetInfoLogARB(self.programId)
+        if log:
+            print('Program:', log)
+        glUseProgramObjectARB(self.programId)
+     
+        errorCheckValue = glGetError()
+        if errorCheckValue != GL_NO_ERROR:
+            print('error creating shaders',
+                  gluErrorString(errorCheckValue))
+            exit(-1)
+    
+    def destroyShaders(self):
+        errorCheckValue = glGetError()
+     
+        glUseProgramObjectARB(0)
+     
+        glDetachObjectARB(self.programId, self.vertexShaderId)
+        glDetachObjectARB(self.programId, self.fragmentShaderId)
+     
+        glDeleteObjectARB(self.fragmentShaderId)
+        glDeleteObjectARB(self.vertexShaderId)
+     
+        glDeleteObjectARB(self.programId)
+     
+        errorCheckValue = glGetError()
+        if errorCheckValue != GL_NO_ERROR:
+            print('error destroying shaders',
+                  gluErrorString(errorCheckValue))
+            exit(-1)
+        
+    
+    def initStaticPolygonVBO(self):
+        '''create and fill buffer objects for static polygons'''
+        
+        # create vertex and color arrays
+        vertices = array([], dtype=float32)        
+        colors = array([], dtype=float32)
+        
+        self.staticVerticesCount = 0
+        #for polygon in self.gameMap.getStaticPolygons():
+        for polygon in self.gameMap.getPolygons():
+            if len(polygon.getPoints3D()) == 4:
+                r, g, b = polygon.getFillColorTuple()
+                for point in polygon.getPoints3D():
+                    append(vertices, [point.x, point.y, point.z])
+                    append(colors, [r/255, g/255, b/255])
+                    self.staticVerticesCount += 1
+        
+        errorCheckValue = glGetError()
+         
+        self.vaoId = glGenVertexArrays(1)
+        glBindVertexArray(self.vaoId)
+     
+        self.vboId = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vboId)
+        glBufferData(GL_ARRAY_BUFFER, len(vertices) * 4, vertices,
+                     GL_STATIC_DRAW)
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, None)
+        glEnableVertexAttribArray(0)
+        
+
+        self.attributeColorIndex = glGetAttribLocation(self.programId,
+                                                       b'in_color')
+        
+        self.colorBufferId = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.colorBufferId)
+        glBufferData(GL_ARRAY_BUFFER, len(colors) * 4, colors,
+                     GL_STATIC_DRAW)
+        glVertexAttribPointer(self.attributeColorIndex,
+                              4, GL_FLOAT, GL_FALSE, 0, None)
+        glEnableVertexAttribArray(self.attributeColorIndex)
+     
+        errorCheckValue = glGetError()
+        if errorCheckValue != GL_NO_ERROR:
+            print('error creating VBOs',
+                  gluErrorString(errorCheckValue))
+            exit(-1)
+    
+    def destroyStaticPolygonsVBO(self):
+        errorCheckValue = glGetError()
+     
+        glDisableVertexAttribArray(self.attributeColorIndex)
+        glDisableVertexAttribArray(0)
+         
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+     
+        glDeleteBuffers(1, [self.colorBufferId])
+        glDeleteBuffers(1, [self.vboId])
+     
+        glBindVertexArray(0)
+        glDeleteVertexArrays(1, [self.vaoId])
+     
+        errorCheckValue = glGetError()
+        if errorCheckValue != GL_NO_ERROR:
+            print('error destroying VBOs',
+                  gluErrorString(errorCheckValue))
+            exit(-1);
 
     def draw(self):
         player = self.client.getPlayer()
@@ -57,22 +218,7 @@ class PygameViewAndInput(Thread):
         glRotatef((player.getViewAngle() + pi/2) * 180 / pi, 0.0, 1.0, 0.0)
         glTranslatef(-playerPostion.x, -playerPostion.y, -playerPostion.z)
         
-        glBegin(GL_QUADS)
-        
-        for polygon in self.gameMap.getPolygons():
-            distancePolygonPlayer = getSquaredPointDistance(playerPostion,
-                                                            polygon.getCenter())
-            if polygon.facesPoint(playerPostion) and \
-                distancePolygonPlayer < 90000 and \
-                len(polygon.getPoints3D()) == 4:
-                r, g, b = polygon.getFillColorTuple()
-                r, g, b = mixColors((r, g, b),
-                                    (0, 0, 0),
-                                    distancePolygonPlayer / 90000.0)
-                for point in polygon.getPoints3D():
-                    glColor3f(r/255, g/255, b/255)
-                    glVertex3f(point.x, point.y, point.z)
-        glEnd()
+        glDrawArrays(GL_QUADS, 0, self.staticVerticesCount)
 
     
     def processEvents(self, events):
@@ -147,5 +293,7 @@ class PygameViewAndInput(Thread):
         self.running = True
         while self.running:
             runAndWait(self.runBody, self.millisecondsPerFrame)
+        
+        self.cleanup()
         
         pygame.quit()
