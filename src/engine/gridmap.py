27fdd08c989549e00b6719bd615a2f115e8e5fc8
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2013-01-22 20:40:12 +0100

    GridMap update and unit test

--- src/engine/gridmap.py
+++ src/engine/gridmap.py
@@ -5,79 +5,94 @@ from engine.polygon import Polygon
 from engine.mapobjects.door import Door
 
 class GridMap(GameMap):
-    def __init__(self, gameManager):
+    def __init__(self, gameManager, grid=None, edgeLength=1.0):
+        '''
+        grid example:
+        ~~~~~~~~~~~~~~~~~~~~
+               -----x/j--->
+
+        grid = [[0,0,0,0,0],  |
+                [0,2,2,2,0],  |
+                [0,0,1,2,0],  z/i
+                [0,0,2,2,0],  |
+                [0,0,0,0,0]]  v
+        
+        '''
         GameMap.__init__(self, gameManager)
         
         self.groundColor = '#7d7d7d'
         self.skyColor = '#515151'
         
+        self.edgeLength = edgeLength
         self.startPosition = Point3D(0.0, 0.0, 0.0)
-        self.wallBottom = -7.5
-        self.wallTop = 7.5
+        self.wallBottom = -self.edgeLength / 2
+        self.wallTop = self.edgeLength / 2
         
-        self.edgeLength = 15
+        self.grid = None
         
-        self.mapGenerator = MapGenerator(30, 30, seed=1)
-        #self.mapGenerator = MapGenerator(50, 50, seed=2)
-        
-        self.grid = self.mapGenerator.generateMap()
-        
-        #example:
-        #~~~~~~~~~~~~~~~~~~~~
-        #             <-- z/j--+
-        #                      |
-        #grid = [[0,0,0,0,0],  |
-        #        [0,2,2,2,0],  |
-        #        [0,0,1,2,0],  x/i
-        #        [0,0,2,2,0],  |
-        #        [0,0,0,0,0]]  v
-        
+        if grid is None:
+            #self.mapGenerator = MapGenerator(30, 30, seed=1)
+            self.mapGenerator = MapGenerator(100, 100, seed=1)
+            
+            self.grid = self.mapGenerator.generateMap()
+        else:
+            self.grid = grid
+
+
+        #DEBUG start
+        #self.grid = [[1,1,1,1],
+        #             [1,2,2,1],
+        #             [1,1,2,1]]
+        # 
+        #DEBUG end
+
+
         # make walls towards these map grid values
         self.makeWallsTo = set([1, 's', '#'])
         
-        for i in range(len(self.grid)):          # i corresponds to x axis
-            for j in range(len(self.grid[i])):   # j corresponds to z axis
+        for i in range(len(self.grid)):          # i corresponds to z axis
+            for j in range(len(self.grid[i])):   # j corresponds to x axis
                 if self.grid[i][j] == 2:     # wall
                     if i > 0 and self.grid[i-1][j] in self.makeWallsTo:
-                        self.addWall(i * self.edgeLength,
-                                     j * self.edgeLength,
-                                     0, self.edgeLength)
+                        self.addWall((j + 1) * self.edgeLength,
+                                     i * self.edgeLength,
+                                     -self.edgeLength, 0)
                     if i < len(self.grid) - 1 and \
                             self.grid[i+1][j] in self.makeWallsTo:
-                        self.addWall((i + 1) * self.edgeLength,
-                                     (j + 1) * self.edgeLength,
-                                     0, -self.edgeLength)
+                        self.addWall(j * self.edgeLength,
+                                     (i + 1) * self.edgeLength,
+                                     self.edgeLength, 0)
 
                     if j > 0 and self.grid[i][j-1] in self.makeWallsTo:
-                        self.addWall((i + 1) * self.edgeLength,
-                                     j * self.edgeLength,
-                                     -self.edgeLength, 0)
+                        self.addWall(j * self.edgeLength,
+                                     i * self.edgeLength,
+                                     0, self.edgeLength)
                     if j < len(self.grid[i]) - 1 and\
                             self.grid[i][j+1] in self.makeWallsTo:
-                        self.addWall(i * self.edgeLength,
-                                     (j + 1) * self.edgeLength,
-                                     self.edgeLength, 0)
+                        self.addWall((j + 1) * self.edgeLength,
+                                     (i + 1) * self.edgeLength,
+                                     0, -self.edgeLength)
                 
                 elif self.grid[i][j] == 1:     #floor and ceiling
-                    self.addFloor(i, j)
-                    self.addCeiling(i, j)
+                    self.addFloor(j, i)
+                    self.addCeiling(j, i)
 
                 elif self.grid[i][j] == '#':     #door
-                    self.addFloor(i, j)
-                    self.addCeiling(i, j)
+                    self.addFloor(j, i)
+                    self.addCeiling(j, i)
                     rotation = 1
                     if self.grid[i-1][j] in self.makeWallsTo and \
                             self.grid[i+1][j] in self.makeWallsTo:
                         rotation = 0
                     self.mapObjectsManager.addMapObject(
                             Door(self.gameManager,
-                                 self, (i, j), self.edgeLength, rotation))
+                                 self, (j, i), self.edgeLength, rotation))
                 elif self.grid[i][j] == 's':     #start position
-                    self.addFloor(i, j)
-                    self.addCeiling(i, j)
-                    self.startPosition = Point3D((0.5 + i) * self.edgeLength,
+                    self.addFloor(j, i)
+                    self.addCeiling(j, i)
+                    self.startPosition = Point3D((0.5 + j) * self.edgeLength,
                                                  0.0,
-                                                 (0.5 + j) * self.edgeLength)
+                                                 (0.5 + i) * self.edgeLength)
 
 
     def addWall(self, x, z, xDelta, zDelta, fill='#805319', outline='#5e411c'):
@@ -101,10 +116,10 @@ class GridMap(GameMap):
         maxZ = (z + 1) * self.edgeLength
         floorPolygon = Polygon('',
                    [
+                    Point3D(minX, self.wallBottom, minZ),
                     Point3D(minX, self.wallBottom, maxZ),
                     Point3D(maxX, self.wallBottom, maxZ),
-                    Point3D(maxX, self.wallBottom, minZ),
-                    Point3D(minX, self.wallBottom, minZ)
+                    Point3D(maxX, self.wallBottom, minZ)
                    ],
                    self.groundColor)
         self.polygons.append(floorPolygon)
@@ -116,10 +131,10 @@ class GridMap(GameMap):
         maxZ = (z + 1) * self.edgeLength
         floorPolygon = Polygon('',
                    [
-                    Point3D(minX, self.wallTop, maxZ),
                     Point3D(minX, self.wallTop, minZ),
                     Point3D(maxX, self.wallTop, minZ),
-                    Point3D(maxX, self.wallTop, maxZ)
+                    Point3D(maxX, self.wallTop, maxZ),
+                    Point3D(minX, self.wallTop, maxZ)
                    ],
                    self.groundColor)
         self.polygons.append(floorPolygon)
---
+++ src/tests/gridmaptest.py
@@ -0,0 +1,286 @@
+'''
+Created on Jan 21, 2013
+
+@author: marcel
+'''
+import unittest
+from engine.gridmap import GridMap
+from engine.coordinate import Point3D
+
+
+class GridMapTest(unittest.TestCase):
+
+
+    def testFloorAndCeiling(self):
+        grid = [[1]]
+        
+        gridMap = GridMap(None, grid, 1.0)
+        
+        polygons = gridMap.getStaticPolygons()
+        self.assertEqual(len(polygons), 2)
+        
+        targetPointsList = [
+                            [(0.0, -0.5, 0.0),    # floor
+                             (0.0, -0.5, 1.0),
+                             (1.0, -0.5, 1.0),
+                             (1.0, -0.5, 0.0)],
+                            
+                            [(0.0, 0.5, 0.0),    # ceiling
+                             (1.0, 0.5, 0.0),
+                             (1.0, 0.5, 1.0),
+                             (0.0, 0.5, 1.0)]
+                            ]
+        
+        for polygon in polygons:
+            points = polygon.getPoints3D()
+            self.assertEqual(len(points), 4)
+            pointTuples = [(points[0].x, points[0].y, points[0].z),
+                            (points[1].x, points[1].y, points[1].z),
+                            (points[2].x, points[2].y, points[2].z),
+                            (points[3].x, points[3].y, points[3].z)]
+            
+            
+            targetPointsFound = False
+            for targetPoints in targetPointsList:
+                if pointTuples[0] in targetPoints and \
+                        pointTuples[1] in targetPoints and \
+                        pointTuples[2] in targetPoints and \
+                        pointTuples[3] in targetPoints:    # all points equal
+                    
+                    #check order
+                    index = targetPoints.index(pointTuples[0])
+                    self.assertEqual(targetPoints.index(pointTuples[1]),
+                                     (index + 1) % 4, 'points in wrong order')
+                    targetPointsFound = True
+                    break
+            
+            self.assertTrue(targetPointsFound,
+                            'No corresponding points group found for polygon')
+    
+    def testWall12(self):
+        grid = [[1, 2]]
+        
+        gridMap = GridMap(None, grid, 1.0)
+        
+        polygons = gridMap.getStaticPolygons()
+        self.assertEqual(len(polygons), 3)
+        
+        targetPointsList = [
+                            [(0.0, -0.5, 0.0),    # floor
+                             (0.0, -0.5, 1.0),
+                             (1.0, -0.5, 1.0),
+                             (1.0, -0.5, 0.0)],
+                            
+                            [(0.0, 0.5, 0.0),    # ceiling
+                             (1.0, 0.5, 0.0),
+                             (1.0, 0.5, 1.0),
+                             (0.0, 0.5, 1.0)],
+                            
+                            [(1.0, -0.5, 0.0),    # wall
+                             (1.0, -0.5, 1.0),
+                             (1.0,  0.5, 1.0),
+                             (1.0,  0.5, 0.0)]
+                            ]
+        
+        for polygon in polygons:
+            points = polygon.getPoints3D()
+            self.assertEqual(len(points), 4)
+            pointTuples = [(points[0].x, points[0].y, points[0].z),
+                            (points[1].x, points[1].y, points[1].z),
+                            (points[2].x, points[2].y, points[2].z),
+                            (points[3].x, points[3].y, points[3].z)]
+            
+            targetPointsFound = False
+            for targetPoints in targetPointsList:
+                if pointTuples[0] in targetPoints and \
+                        pointTuples[1] in targetPoints and \
+                        pointTuples[2] in targetPoints and \
+                        pointTuples[3] in targetPoints:    # all points equal
+                    
+                    #check order
+                    index = targetPoints.index(pointTuples[0])
+                    self.assertEqual(targetPoints.index(pointTuples[1]),
+                                     (index + 1) % 4, 'points in wrong order')
+                    targetPointsFound = True
+                    break
+            
+            self.assertTrue(targetPointsFound,
+                            'No corresponding points group found for polygon')
+    
+    def testWall21(self):
+        grid = [[2, 1]]
+        
+        gridMap = GridMap(None, grid, 1.0)
+        
+        polygons = gridMap.getStaticPolygons()
+        self.assertEqual(len(polygons), 3)
+        
+        targetPointsList = [
+                            [(1.0, -0.5, 0.0),    # floor
+                             (1.0, -0.5, 1.0),
+                             (2.0, -0.5, 1.0),
+                             (2.0, -0.5, 0.0)],
+                            
+                            [(1.0, 0.5, 0.0),    # ceiling
+                             (2.0, 0.5, 0.0),
+                             (2.0, 0.5, 1.0),
+                             (1.0, 0.5, 1.0)],
+                            
+                            [(1.0, -0.5, 1.0),    # wall
+                             (1.0, -0.5, 0.0),
+                             (1.0,  0.5, 0.0),
+                             (1.0,  0.5, 1.0)]
+                            ]
+        
+        for polygon in polygons:
+            points = polygon.getPoints3D()
+            self.assertEqual(len(points), 4)
+            pointTuples = [(points[0].x, points[0].y, points[0].z),
+                            (points[1].x, points[1].y, points[1].z),
+                            (points[2].x, points[2].y, points[2].z),
+                            (points[3].x, points[3].y, points[3].z)]
+            
+            targetPointsFound = False
+            for targetPoints in targetPointsList:
+                if pointTuples[0] in targetPoints and \
+                        pointTuples[1] in targetPoints and \
+                        pointTuples[2] in targetPoints and \
+                        pointTuples[3] in targetPoints:    # all points equal
+                    
+                    #check order
+                    index = targetPoints.index(pointTuples[0])
+                    self.assertEqual(targetPoints.index(pointTuples[1]),
+                                     (index + 1) % 4, 'points in wrong order')
+                    targetPointsFound = True
+                    break
+            
+            self.assertTrue(targetPointsFound,
+                            'No corresponding points group found for polygon')
+    
+    def testWall1over2(self):
+        grid = [[1], [2]]
+        
+        gridMap = GridMap(None, grid, 1.0)
+        
+        polygons = gridMap.getStaticPolygons()
+        self.assertEqual(len(polygons), 3)
+        
+        targetPointsList = [
+                            [(0.0, -0.5, 0.0),    # floor
+                             (0.0, -0.5, 1.0),
+                             (1.0, -0.5, 1.0),
+                             (1.0, -0.5, 0.0)],
+                            
+                            [(0.0, 0.5, 0.0),    # ceiling
+                             (1.0, 0.5, 0.0),
+                             (1.0, 0.5, 1.0),
+                             (0.0, 0.5, 1.0)],
+                            
+                            [(1.0, -0.5, 1.0),    # wall
+                             (0.0, -0.5, 1.0),
+                             (0.0,  0.5, 1.0),
+                             (1.0,  0.5, 1.0)]
+                            ]
+        
+        for polygon in polygons:
+            points = polygon.getPoints3D()
+            self.assertEqual(len(points), 4)
+            pointTuples = [(points[0].x, points[0].y, points[0].z),
+                            (points[1].x, points[1].y, points[1].z),
+                            (points[2].x, points[2].y, points[2].z),
+                            (points[3].x, points[3].y, points[3].z)]
+            
+            targetPointsFound = False
+            for targetPoints in targetPointsList:
+                if pointTuples[0] in targetPoints and \
+                        pointTuples[1] in targetPoints and \
+                        pointTuples[2] in targetPoints and \
+                        pointTuples[3] in targetPoints:    # all points equal
+                    
+                    #check order
+                    index = targetPoints.index(pointTuples[0])
+                    self.assertEqual(targetPoints.index(pointTuples[1]),
+                                     (index + 1) % 4, 'points in wrong order')
+                    targetPointsFound = True
+                    break
+            
+            self.assertTrue(targetPointsFound,
+                            'No corresponding points group found for polygon')
+    
+    
+    def testWall2over1(self):
+        grid = [[2], [1]]
+        
+        gridMap = GridMap(None, grid, 1.0)
+        
+        polygons = gridMap.getStaticPolygons()
+        self.assertEqual(len(polygons), 3)
+        
+        targetPointsList = [
+                            [(0.0, -0.5, 1.0),    # floor
+                             (0.0, -0.5, 2.0),
+                             (1.0, -0.5, 2.0),
+                             (1.0, -0.5, 1.0)],
+                            
+                            [(0.0, 0.5, 1.0),    # ceiling
+                             (1.0, 0.5, 1.0),
+                             (1.0, 0.5, 2.0),
+                             (0.0, 0.5, 2.0)],
+                            
+                            [(0.0, -0.5, 1.0),    # wall
+                             (1.0, -0.5, 1.0),
+                             (1.0,  0.5, 1.0),
+                             (0.0,  0.5, 1.0)]
+                            ]
+        
+        for polygon in polygons:
+            points = polygon.getPoints3D()
+            self.assertEqual(len(points), 4)
+            pointTuples = [(points[0].x, points[0].y, points[0].z),
+                            (points[1].x, points[1].y, points[1].z),
+                            (points[2].x, points[2].y, points[2].z),
+                            (points[3].x, points[3].y, points[3].z)]
+            
+            targetPointsFound = False
+            for targetPoints in targetPointsList:
+                if pointTuples[0] in targetPoints and \
+                        pointTuples[1] in targetPoints and \
+                        pointTuples[2] in targetPoints and \
+                        pointTuples[3] in targetPoints:    # all points equal
+                    
+                    #check order
+                    index = targetPoints.index(pointTuples[0])
+                    self.assertEqual(targetPoints.index(pointTuples[1]),
+                                     (index + 1) % 4, 'points in wrong order')
+                    targetPointsFound = True
+                    break
+            
+            self.assertTrue(targetPointsFound,
+                            'No corresponding points group found for polygon')
+    
+    
+    def testStartPosition(self):
+        grid = [['s']]
+        
+        gridMap = GridMap(None, grid)
+        gridMap.edgeLength = 1
+        
+        position = gridMap.getStartPosition()
+        self.assertEqual(position.x, 0.5)
+        self.assertEqual(position.z, 0.5)
+    
+    def testStartPosition2(self):
+        grid = [[1, 's']]
+        
+        gridMap = GridMap(None, grid)
+        gridMap.edgeLength = 1
+        
+        position = gridMap.getStartPosition()
+        self.assertEqual(position.x, 1.5)
+        self.assertEqual(position.z, 0.5)
+                
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
