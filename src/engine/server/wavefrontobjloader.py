e9d6a39f8eb3c68691690327e558c7dc28611e33
Marcel Pfeiffer <pfeiffer.marcel@gmx.de>
2013-01-31 12:41:29 +0100

    skeleton for wavefront obj file loader

---
+++ src/engine/server/wavefrontobjloader.py
@@ -0,0 +1,16 @@
+class WavefrontObjLoader(object):
+    def __init__(self, objFilename):
+        self.objFilename = objFilename
+        
+        self.vertices = []
+        self.faces = []
+        self.normals = []
+        self.uvCoordinates = []
+
+
+    def parseFile(self):
+        pass
+
+
+    def getPolygons(self):
+        pass
